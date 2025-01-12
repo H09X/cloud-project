00b1cc152a73d0752c153741908a37c66bc839e5
H09X <135832212+H09X@users.noreply.github.com>
2025-01-12 19:58:09 +0300

    Update scripts.js

--- dist/scripts.js
+++ dist/scripts.js
@@ -17,7 +17,7 @@ void 0===c?d&&"get"in d&&null!==(e=d.get(a,b))?e:(e=n.find.attr(a,b),null==e?voi
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification, 
- * are permitted provided that the following conditions are met:
+ * are permitted provided that the following conditions are met: 
  * 
  * Redistributions of source code must retain the above copyright notice, this list of 
  * conditions and the following disclaimer.
@@ -36,7 +36,7 @@ void 0===c?d&&"get"in d&&null!==(e=d.get(a,b))?e:(e=n.find.attr(a,b),null==e?voi
  *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
  * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
- * OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * OF THE POSSIBILITY OF SUCH DAMAGE.  
  *
 */
 
